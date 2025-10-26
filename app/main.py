87590cab1edda90fbb5014e71ee3b96c777631cc
Eric Feng <ericyfen@usc.edu>
2025-10-25 21:59:04 -0700

    changed CORS

--- app/main.py
+++ app/main.py
@@ -26,9 +26,13 @@ from .notion_client import (
 app = FastAPI(title="Moo Backend")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization"],
 )
 
 # lifestyle
